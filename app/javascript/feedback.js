d5e94115996c5beeff668feda02ec0196706b1be
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2024-11-15 15:51:04 +0000

    reviews

---
+++ app/javascript/feedback.js
@@ -0,0 +1,15 @@
+function redirectToPage(){
+window.location.href = "https://search.google.com/local/writereview?placeid=ChIJqWhZCwDLQQwRTyWEGKzPClw"
+};
+
+function showCommentSection() {
+    const comment = document.getElementsByClassName("comment")[0]; 
+    if (comment) {
+        comment.style.display = "block";
+        console.error("Comment element not found in the DOM");
+    }
+}
+
+function redirectionToWeb(){
+    window.location.href = "https://smartmenu.agorapos.com/?id=8yhqy9t0#/"
+}
