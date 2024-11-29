f1fb08b7834da788ddbe062d5140db7ef1ac9ddd
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2024-11-29 14:05:11 +0000

    chande png icon

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_11_28_152527) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "feedbacks", force: :cascade do |t|
     t.integer "rating"
     t.string "client_name"
