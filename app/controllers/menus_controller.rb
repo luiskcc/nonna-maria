160b689f2272546b08f1e8a4c519cf2031f4f949
Luis <114909465+luiskcc@users.noreply.github.com>
2024-12-03 08:08:41 +0000

    Update menus_controller.rb

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,5 +1,5 @@
 class MenusController < ApplicationController
   def index
-    redirect_to "https://smartmenu.agorapos.com/?id=8yhqy9t0#/"
+    redirect_to "https://smartmenu.agorapos.com/?id=8yhqy9t0#/", allow_other_host: true
   end
 end
