64608a7ab71ff1bdf92779d24e9c995debd53bc0
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2024-12-03 15:08:52 +0000

    added controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   post 'feedbacks/create', to: 'feedbacks#create'
 
   resources :feedbacks, only: [:create]
-  resources :menus, only: [:index]
+  resources :menu, only: [:index]
 end
