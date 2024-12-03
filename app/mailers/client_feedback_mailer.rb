7ff5fc7033ef1c324fd48246c98a2cf7525209e5
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2024-12-03 21:34:22 +0000

    added email recipients

--- app/mailers/client_feedback_mailer.rb
+++ app/mailers/client_feedback_mailer.rb
@@ -1,6 +1,8 @@
 class ClientFeedbackMailer < ApplicationMailer
     default from: ENV['SMTP_USERNAME']
 
+    recipients = [ENV['ADMIN_EMAIL'], ENV['ADMIN_EMAIL_2'], ENV['ADMIN_EMAIL_3'], ENV['ADMIN_EMAIL_4']].compact
+
     def new_feedback_email(feedback_data)
         puts "feedback_data: #{feedback_data}"
         @rating = feedback_data[:rating]
@@ -8,7 +10,7 @@ class ClientFeedbackMailer < ApplicationMailer
         @email_address = feedback_data[:email_address]
         @comment = feedback_data[:comment]
         if @rating.present? && @client_name.present?
-            mail(to: ENV['ADMIN_EMAIL'], subject: "⚠️ Nuevo Comentarion de  *##{@rating} ESTRELLAS de: #{@client_name}")
+            mail(to: recipients, subject: "⚠️ Nuevo Comentarion de  *##{@rating} ESTRELLAS de: #{@client_name}")
         else
             puts "No se pudo enviar el correo"
         end
