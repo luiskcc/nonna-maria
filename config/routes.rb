fdb87d71d8ca4c264b8a46a8dbfe35d208a8c4db
Luis <114909465+luiskcc@users.noreply.github.com>
2024-12-03 08:01:30 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'feedbacks/compromiso'# or 'feedback/politica' if renamed
   root "feedbacks#index"
   post 'feedbacks/create', to: 'feedbacks#create'
+  get 'menus', to: 'menus#index'
 
   resources :feedbacks, only: [:create]
 end
