247ebb1b5f9320fc7f3d7eee6a9ae424890e5cdb
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2024-12-04 08:22:40 +0000

    fixed syntax

--- app/mailers/client_feedback_mailer.rb
+++ app/mailers/client_feedback_mailer.rb
@@ -1,7 +1,14 @@
 class ClientFeedbackMailer < ApplicationMailer
     default from: ENV['SMTP_USERNAME']
 
-    recipients = [ENV['ADMIN_EMAIL'], ENV['ADMIN_EMAIL_2'], ENV['ADMIN_EMAIL_3'], ENV['ADMIN_EMAIL_4']].compact
+
+    ADMIN_RECIPIENTS = [
+        ENV['ADMIN_EMAIL'],
+        ENV['ADMIN_EMAIL_2'],
+        ENV['ADMIN_EMAIL_3'],
+        ENV['ADMIN_EMAIL_4']
+    ].compact
+
 
     def new_feedback_email(feedback_data)
         puts "feedback_data: #{feedback_data}"
@@ -10,7 +17,10 @@ class ClientFeedbackMailer < ApplicationMailer
         @email_address = feedback_data[:email_address]
         @comment = feedback_data[:comment]
         if @rating.present? && @client_name.present?
-            mail(to: recipients, subject: "⚠️ Nuevo Comentarion de  *##{@rating} ESTRELLAS de: #{@client_name}")
+            mail(
+                to: ADMIN_RECIPIENTS,
+                subject: "⚠️ Nuevo Comentarion de  *##{@rating} ESTRELLAS de: #{@client_name}"
+            )
         else
             puts "No se pudo enviar el correo"
         end
